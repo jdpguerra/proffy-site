523539706e4ecb1750d8d55eca24cd8db2774a65
jdpguerra <josedavidportillo@gmail.com>
2020-08-12 23:33:06 -0300

    4 dia
    NLW - Ajuste SRC/PUBLIC e VIEWS

---
+++ src/server.js
@@ -0,0 +1,32 @@
+function pageLanding(req,res){
+  return res.render("index.html")
+}
+
+function pageStudy(req,res){
+  return res.render("study.html")
+}
+
+function pageGiveClasses(req,res){
+  return res.render("give-classes.html")
+}
+
+const express = require('express')
+const server = express()
+
+//const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+
+ const nunjucks = require('nunjucks')
+ nunjucks.configure('src/views', {
+   express: server,
+   noCache: true,
+ })
+
+server
+.use(express.urlencoded({ extended: true }))
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+//.post("/save-classes", saveClasses)
+
+.listen(5500)
